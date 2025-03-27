852d0dfeaa1ab22c7ba2d68d0827b41375d95980
Pramod Vilakattu Ramachandran Nair(UST <U22229@ust.com>
2025-03-27 16:37:20 +0530

    remove unwanted dir

--- src/main/java/com/example/MyBean.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public class MyBean {
-    public void sayHello() {
-        System.out.println("Hello from MyBean!");
-    }
-}
--- src/main/java/com/test/TClass.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-public class TClass {
-	
-	   public void sayHello() {
-	        System.out.println("Hello from Tclass!");
-	    }
-
-}
