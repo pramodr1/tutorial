066dc6275721b18650c087f762dd2a5768f96829
pramodr1 <32468039+pramodr1@users.noreply.github.com>
2025-03-27 15:55:11 +0530

    Add files via upload

---
+++ webprj/src/main/java/com/example/MyBean.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class MyBean {
+    public void sayHello() {
+        System.out.println("Hello from MyBean!");
+    }
+}
---
+++ webprj/src/main/java/com/test/TClass.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class TClass {
+	
+	   public void sayHello() {
+	        System.out.println("Hello from Tclass!");
+	    }
+
+}
